dec18aacb472dc9335473c9934cb11ee21986699
peleide <peleide216@hotmail.com>
2020-02-18 16:21:50 -0500

    Small modif

--- src/com/git/example/Test.java
+++ src/com/git/example/Test.java
@@ -2,6 +2,6 @@ package com.git.example;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Test new project");
+		System.out.println("Test new project modified");
 	}
 }
