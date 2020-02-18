d6ddc992ababfebb0af092198f4159e9f683109c
peleide <peleide216@hotmail.com>
2020-02-18 16:41:19 -0500

    Another small midif

--- src/com/git/example/Test.java
+++ src/com/git/example/Test.java
@@ -3,6 +3,6 @@ package com.git.example;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Test new project modification 1");
-		System.out.println("Test new project modified 2");
+		System.out.println("Test new project modified 2 by gcb");
 	}
 }
