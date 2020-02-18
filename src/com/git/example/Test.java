047722db5e6e3e6c41eca2021d0a149c5b0d7f5d
peleide <peleide216@hotmail.com>
2020-02-18 16:27:11 -0500

    Petite modification

--- src/com/git/example/Test.java
+++ src/com/git/example/Test.java
@@ -2,6 +2,6 @@ package com.git.example;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Test new project modified");
+		System.out.println("Test new project modification 1");
 	}
 }
