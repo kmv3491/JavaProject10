567094ee5c4cae6d26e1cad36d1bc0dba8fda93e
peleide <peleide216@hotmail.com>
2020-02-18 20:45:47 -0500

    Modification de la structure du projet Ajout d'une fonction Fichier Test.java renommé en Test01.java

--- src/com/git/example/test01/Test01.java
+++ src/com/git/example/test01/Test01.java
@@ -1,6 +1,14 @@
 package com.git.example;
 
 public class Test {
+
+
+	public static void displayString message(){
+	
+		System.out.println(message);
+			
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Test new project modification 1");
 		System.out.println("Test new project modified 2 by gcb");
