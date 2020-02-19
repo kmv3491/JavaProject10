333f92a2d659c6a8e853cc17b764e3a17665e8c6
peleide <peleide216@hotmail.com>
2020-02-19 00:28:43 -0500

    Ajout de constructeurs Modification de la fonction print ( private et fonction d'instance)

--- src/com/git/example/tests/Test01.java
+++ src/com/git/example/tests/Test01.java
@@ -2,12 +2,34 @@ package com.git.example.tests;
 
 public class Test01 {
 
-	//fonction 	qui affiche un message en console	
-	public static void print(String message){
+	private String message;
+
+	public Test01() {
+		this.message = "";
+	}
+
+	public Test01(String message) {
+		this.message = message;
+	}
+
+	public final String getMessage() {
+		return message;
+	}
+
+	public final void setMessage(String message) {
+		this.message = message;
+	}
+
+	// fonction qui affiche un message en console
+	private void print(String message) {
 		System.out.println(message);
 	}
 
 	public static void main(String[] args) {
-		print("Test new project modified 2 by gcb");
+
+		Test01 test = new Test01("Bonjour les amis!");
+		test.print(test.getMessage());
+
+		test.print("Test new project modified 2 by gcb");
 	}
 }
