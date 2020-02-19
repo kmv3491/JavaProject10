1f1d4dd3d15f328005139de90ca64bc6768e6cac
peleide <peleide216@hotmail.com>
2020-02-19 00:22:17 -0500

    Ajout de la fonction affiche

--- src/com/git/example/tests/Test02.java
+++ src/com/git/example/tests/Test02.java
@@ -4,6 +4,10 @@ package com.git.example.tests;
 public class Test02 {
 
 
+	private static void affiche(String message) {
+		System.out.println(message);
+	}
+	
 	public static void main(String [] args){
 		affiche("Test 2 de java");
 	}
